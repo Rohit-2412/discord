495dbf59149f992abf48cef6e7fd802367f40f28
Rohit <rk212300@outlook.com>
2023-12-05 14:47:47 +0530

    mobile-toggle done

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,12 +1,47 @@
-"use client";
+import { redirect, useParams } from "next/navigation";
 
+import { ChatHeader } from "@/components/chat/chat-header";
 import React from "react";
-import { useParams } from "next/navigation";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
 
-const ChannelPage = () => {
-    const params = useParams();
-    const { channelId } = params;
-    return <div>Channel Page for :{channelId}</div>;
+interface ChannelPageProps {
+    params: {
+        serverId: string;
+        channelId: string;
+    };
+}
+
+const ChannelPage = async ({ params }: ChannelPageProps) => {
+    const profile = await currentProfile();
+
+    if (!profile) return redirectToSignIn();
+
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId,
+        },
+    });
+
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.serverId,
+            profileId: profile.id,
+        },
+    });
+
+    if (!channel || !member) return redirect("/");
+
+    return (
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+            <ChatHeader
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel"
+            />
+        </div>
+    );
 };
 
 export default ChannelPage;
